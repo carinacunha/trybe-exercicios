130929d222716b3a35a056e058e856069e915103
Carina Cunha <carina.cunha13@gmail.com>
2022-06-08 17:51:03 -0300

    Exercício 1 - Parte II

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio-1-parte-2.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio-1-parte-2.js
@@ -1,5 +1,6 @@
 //1.Crie uma função que receba um número e retorne seu fatorial.
 
+// Primeira forma
 // function fatorial (number){
 //   let factorial = number;
 //   for(let n = 1; n < number; n +=1){
@@ -8,7 +9,7 @@
 //   console.log(factorial);
 // }
  
-
+// Solução usando o ternary operator
 const fatorial = (number) =>((number === 0) ? (1) : number * fatorial(number-1));
 
 console.log(fatorial(3));
